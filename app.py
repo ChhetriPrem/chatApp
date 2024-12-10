bcc41bfb476fa17c6aabf4154cfb9061060236d1
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2024-12-10 13:28:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,30 +28,28 @@ def on_disconnect():
     user = users.pop(request.sid, None)
     if user:
         logging.debug(f"{user} disconnected")
-        emit('message', {"username": "Server", "text": f"{user} has left the chat."}, broadcast=True)
+        emit('message', {"username": "Server", "text": f"{user} has left the chat.", "type": "text"}, broadcast=True)
 
 @socketio.on('set_username')
 def set_username(username):
     users[request.sid] = username
     logging.debug(f"{username} set as username")
-    emit('message', {"username": "Server", "text": f"{username} has joined the chat."}, broadcast=True)
+    emit('message', {"username": "Server", "text": f"{username} has joined the chat.", "type": "text"}, broadcast=True)
 
 @socketio.on('message')
 def handle_message(msg):
     username = users.get(request.sid, "Unknown")
     logging.debug(f"Received message from {username}: {msg}")  # Debugging message
-    if not msg.strip():
-        return  # Ignore empty messages
     
-    if msg.get('type') == 'image':
-        # If the message is an image (base64 string), broadcast the image to everyone
+    if not msg.get('type'):
+        return  # Ignore improperly formatted messages
+    
+    if msg['type'] == 'image':
         logging.debug(f"Broadcasting image from {username}")
         emit('message', {'username': username, 'src': msg['src'], 'type': 'image'}, broadcast=True)
-    else:
-        # Otherwise, it's a regular text message
+    elif msg['type'] == 'text' and msg['text'].strip():
         logging.debug(f"Broadcasting text message from {username}: {msg['text']}")
-        formatted_message = {"username": username, "text": msg['text'], "type": 'text'}
-        emit('message', formatted_message, broadcast=True)
+        emit('message', {'username': username, 'text': msg['text'], 'type': 'text'}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
